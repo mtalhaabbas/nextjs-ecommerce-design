38093f5bcf2d380cb881c02f140b72ba85bf7e97
Muhammad Talha Abbas <70336405+mtalhaabbas@users.noreply.github.com>
2021-09-09 11:06:30 +0500

    delete css file

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ export default function Home() {
   return (
     <>
 <h1>Hello there</h1>  
-<h2>Good to see you Sparrow</h2> 
- <h3>Thank you Talha</h3>
- <h4>okkkkkkkkkkkkkkkkkkk</h4>
- <h5>Ok Take care</h5>
+
   </>
   )
 }
